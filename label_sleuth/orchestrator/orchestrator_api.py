065ef3db4ea95154d073257abbe9305824a35e83
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2022-08-23 13:36:48 -0700

    Add Ariel's bug fix manually

--- label_sleuth/orchestrator/orchestrator_api.py
+++ label_sleuth/orchestrator/orchestrator_api.py
@@ -398,14 +398,20 @@ class OrchestratorApi:
 
         logging.info(f"workspace '{workspace_id}' training a model for category id '{category_id}', "
                      f"train_statistics: {train_statistics}")
-        model_id, _ = model.train(train_data=train_data, language=self.config.language,
-                                  done_callback=functools.partial(self._train_done_callback, workspace_id,
-                                                                  category_id, new_iteration_index))
+        # model_id, _ = model.train(train_data=train_data, language=self.config.language,
+        #                           done_callback=functools.partial(self._train_done_callback, workspace_id,
+        #                                                           category_id, new_iteration_index))
+
+        model_id, future = model.train(train_data=train_data, language=self.config.language)
         model_status = model.get_model_status(model_id)
         model_info = ModelInfo(model_id=model_id, model_status=model_status, model_type=model_type,
                                train_statistics=train_statistics, creation_date=datetime.now())
         self.orchestrator_state.add_iteration(workspace_id=workspace_id, category_id=category_id,
                                               model_info=model_info)
+
+        # The train callback is added here to ensure it only runs after the iteration has been added
+        future.add_done_callback(functools.partial(self._train_done_callback, workspace_id, category_id,
+                                                   new_iteration_index))
 
         # The model id is returned almost immediately, but the training is performed in the background. Once training is
         # complete the iteration flow continues in the *_train_done_callback* method
